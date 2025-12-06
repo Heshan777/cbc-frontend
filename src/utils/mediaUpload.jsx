fae7740e9ed1932a0d029a7ed44d800a4f11cf49
Heshan777 <sachinthaweerakoon81@gmail.com>
2025-12-06 22:36:27 +0530

    supabase fixed

--- src/utils/mediaUpload.jsx
+++ src/utils/mediaUpload.jsx
@@ -1,16 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 
 const anonKey =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqeXZzY2NxZHZ6bXNvb3RldnVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxMjg3ODIsImV4cCI6MjA3MTcwNDc4Mn0._UROOdpCU5IlFtwVP8kNktFlXUfo3QU_YwihNh7HQZA";
-const supabaseUrl = "https://jjyvsccqdvzmsootevuf.supabase.co";
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmdG53cG14eHBrY21hc2praWdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2MTE3MjAsImV4cCI6MjA4MDE4NzcyMH0.kOS2HWZbSApgjg9qmyWMFSn-gje0StPXvOm2vF0LgHo";
+const supabaseUrl = "https://lftnwpmxxpkcmasjkigl.supabase.co";
 
 const supabase = createClient(supabaseUrl, anonKey);
 
 
 export default function mediaUpload(file) {
 	return new Promise((resolve, reject) => {
-		if (file == null) {
-			reject("No file selected");
+		if (file == null) { 
 		} else {
             const timestamp = new Date().getTime();
             const fileName = timestamp+file.name
